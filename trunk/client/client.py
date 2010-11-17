be43f91a4e045a40ada1e2b42cebc2d141ca25da
daniel.saul <daniel.saul@1d4a5069-0308-5a73-d597-13d25cdbd855>
2010-11-17 03:04:07 +0000

    Lots of changes. Client now spawns the host if one is not specified on the CLI. A host can take a unconnected client and add it via CLI. More DBus stub methods. Eventually we are going to have proper "Do you want to close" messages. Currently we are working on proper close/open sync behaviour which should be in the next update.

--- trunk/client/client.py
+++ trunk/client/client.py
@@ -1,25 +1,61 @@
 # See LICENCE for the source code licence.
 # (c) 2010 Dan Saul
 
-import pygtk
-pygtk.require('2.0')
-import gobject
+import sys,os,argparse
+import gobject,pygtk,gtk,gio
+import dbus,dbus.service,dbus.mainloop.glib
+import subprocess,threading
 gobject.threads_init ()
-import gtk,gio
-gtk.gdk.threads_init()
-import sys,os
-import dbus
-import dbus.service
-import dbus.mainloop.glib
 
 app = None
 dbusobj = None
+bus = None
+arg_host = None
+prefix_glade_host = "/home/dan/Desktop/Programming/ude/ude-components/host/"
+path_python = "/usr/bin/python2"
+path_host_script = "/home/dan/Desktop/Programming/ude/ude-components/host/host.py"
+
+def main():
+	global app
+	global dbusobj
+	global bus
+	global arg_host
+	
+	# Arguments
+	parser = argparse.ArgumentParser(description='A generic client example.')
+	parser.add_argument('-o','--host',type=int,default=0,help="the PID that this client will embed itself into, if left out will spawn its own host")
+	args = parser.parse_args()
+	arg_host = args.host
+	print args
+	
+	# DBus
+	dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
+	bus = dbus.SessionBus()
+	name = dbus.service.BusName("org.ude.components.client_"+str(os.getpid()), bus)
+	dbusobj = Example(bus,'/org/ude/components/client')
+	
+	print "Client PID",os.getpid()
+	
+	app = App()
+	gtk.main()
+	pass
 
 class App:
 	widget = None # Widget that will be embeded.
 	plug = None   
 	
 	def __init__(self):
+		if arg_host == 0:
+			fd = os.open(os.devnull, os.O_RDWR)
+			env = os.environ.copy()
+			env["GLADE_PREFIX"] = prefix_glade_host
+			subprocess.Popen(['setsid',path_python,path_host_script,"-a",str(os.getpid())],env=env,stdout=fd,stderr=fd)
+		else:
+			print "connect to host"
+			remotehost = bus.get_object("org.ude.components.client_"+str(arg_host),"/org/ude/components/client")
+			
+			
+			
 		pass
 	
 	def prepare(self):
@@ -28,12 +64,10 @@ class App:
 		'''
 		
 		newplug = gtk.Plug(0)
-		newplug.show()
 		
 		if None == self.widget:
 			self.widget = gtk.Button("button")
 			self.widget.connect("clicked",self.button_press)
-			self.widget.show()
 			
 			newplug.add(self.widget)
 		else:
@@ -46,6 +80,8 @@ class App:
 			del self.plug
 		self.plug = newplug
 		
+		self.plug.show_all()
+		
 		print "Prepare ID=",self.plug.get_id()
 		
 		return self.plug.get_id()
@@ -54,6 +90,25 @@ class App:
 		#self.prepare()
 		pass
 	
+	def allow_close(self):
+		print "allow_close()"
+		return True
+	
+	def show_allow_close_prompt(self):
+		print "show_allow_close_prompt()"
+		pass
+	
+	def closed_by_host(self):
+		print "closed_by_host()"
+		gtk.main_quit()
+		pass
+	
+	def get_title(self):
+		return "untitled {0}".format(os.getpid())
+	
+	def get_proxy_icon_path(self):
+		return "/home/dan/Desktop/Programming/ude/ude-components/client/16x16doc.svg"
+		
 	
 class Example(dbus.service.Object):
 	def __init__(self, bus, object_path):
@@ -63,13 +118,35 @@ class Example(dbus.service.Object):
 	def Prepare(self):
 		return app.prepare()
 	
-if __name__ == "__main__":
-	dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
-	system_bus = dbus.SessionBus ()
-	name = dbus.service.BusName("org.ude.components.client_"+str(os.getpid()), system_bus)
-	dbusobj = Example(system_bus, '/org/ude/components/client')
+	@dbus.service.method(dbus_interface='org.ude.components.client', out_signature='b')
+	def AllowClose(self):
+		return app.allow_close()
 	
-	app = App()
-	gtk.main()
+	@dbus.service.method(dbus_interface='org.ude.components.client')
+	def ShowAllowClosePrompt(self):
+		app.show_allow_close_prompt()
+	
+	@dbus.service.method(dbus_interface='org.ude.components.client')
+	def ClosedByHost(self):
+		app.closed_by_host()
+	
+	@dbus.service.method(dbus_interface='org.ude.components.client', out_signature='s')
+	def GetTitle(self):
+		return app.get_title()
+	
+	@dbus.service.method(dbus_interface='org.ude.components.client', out_signature='s')
+	def GetProxyIconPath(self):
+		return app.get_proxy_icon_path()
+	
+	@dbus.service.signal(dbus_interface='org.ude.components.client', signature='s')
+	def TitleChanged(self, title):
+		pass
+	
+	@dbus.service.signal(dbus_interface='org.ude.components.client', signature='s')
+	def ProxyIconChanged(self, path):
+		pass
+
+if __name__ == "__main__":
+	main()
 
 
--- trunk/host/hostclient.py
+++ trunk/host/hostclient.py
@@ -1,22 +1,129 @@
-import pygtk
-pygtk.require('2.0')
-import gobject
-gobject.threads_init ()
-import gtk,gio
-gtk.gdk.threads_init()
+# See LICENCE for the source code licence.
+# (c) 2010 Dan Saul
+
 import sys,os
-import dbus
-import dbus.service
-import dbus.mainloop.glib
+import gobject,pygtk,gtk,gio
+gobject.threads_init ()
+gtk.gdk.threads_init()
+import dbus,dbus.service,dbus.mainloop.glib
 
 class Client:
+	glade_prefix = ""
+	
 	bus = None
 	remote = None
 	
+	builder = None
+	widget = None
+	image = None
+	label = None
+	closebutton = None
+	socket = None
+	crashbox = None
+	
+	has_remote = False
+	
 	def __init__(self,bus,pid):
+		try:
+			self.glade_prefix = os.environ["GLADE_PREFIX"]
+		except KeyError:
+			print "No Glade Environment"
+		
 		self.bus = bus
 		self.remote = self.bus.get_object("org.ude.components.client_"+str(pid),"/org/ude/components/client")
+		
+		self.builder = gtk.Builder()
+		self.builder.add_from_file(self.glade_prefix+"hostclient.glade")
+		
+		self.widget = gtk.VBox()
+		self.widget.add_events(gtk.gdk.STRUCTURE_MASK)
+		self.widget.connect("map",self.__widget_map)
+		
+		self.image = gtk.Image()
+		self.image.set_size_request(16,16)
+		self.image.set_from_file(self.GetProxyIconPath())
+		
+		self.label = gtk.Label()
+		self.label.set_text(self.GetTitle())
+		
+		img = gtk.Image()
+		img.set_size_request(12,12)
+		img.set_from_file("/home/dan/Desktop/Programming/ude/ude-components/host/12x12close.svg")
+		self.closebutton = gtk.ToolButton(img)
+		
+		
+		self.socket = gtk.Socket()
+		self.socket.connect("plug-removed",self.__plug_removed)
+		
+		self.crashbox = self.builder.get_object("crashbox")
+		
+		self.widget.pack_start(self.socket)
+		self.widget.show_all()
 		pass
+	
+	def __widget_map(self,sender):
+		if self.has_remote == False:
+			print "map event"
+			self.socket.add_id(self.Prepare())
+			self.has_remote = True
+		return False
+	
+	def __plug_removed(self,sender):
+		self.has_remote = False
+		self.widget.pack_start(self.crashbox)
+		self.widget.show_all()
+		print "plug-removed"
+		return False
 	
 	def Prepare(self):
 		return self.remote.Prepare(dbus_interface="org.ude.components.client")
+	
+	def AllowClose(self):
+		return self.remote.AllowClose(dbus_interface="org.ude.components.client")
+	
+	def ShowAllowClosePrompt(self):
+		return self.remote.ShowAllowClosePrompt(dbus_interface="org.ude.components.client")
+	
+	def ClosedByHost(self):
+		return self.remote.ClosedByHost(dbus_interface="org.ude.components.client")
+	
+	def GetTitle(self):
+		return self.remote.GetTitle(dbus_interface="org.ude.components.client")
+	
+	def GetProxyIconPath(self):
+		return self.remote.GetProxyIconPath(dbus_interface="org.ude.components.client")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
