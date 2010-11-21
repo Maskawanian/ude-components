dcd4590ce712caa00f2d5da4667054b8f3c223f7
daniel.saul <daniel.saul@gmail.com>
2010-11-21 12:53:31 +0000

    - Address bar works. - Beginning of ad block code.

--- client/webclient.py
+++ client/webclient.py
@@ -7,7 +7,7 @@ import dbus,dbus.service,dbus.mainloop.glib
 import subprocess,threading
 import webkit
 import sexy
-import urllib
+import urllib,urlparse
 gobject.threads_init ()
 
 # Fix missing variables in pywebkit.
@@ -46,10 +46,72 @@ class WebClient(ComponentClient):
 	__uri_entry = None
 	__favicon_path = None
 	
+	tlds = ["aero","asia","biz","cat","com","coop",\
+		"edu","gov","info","int","jobs","mil","mobi",\
+		"museum","name","net","org","pro","tel","travel",\
+		"ac","ad","ae","af","ag","ai","al","am","an",\
+		"ao","aq","ar","as","at","au","aw","ax","az",\
+		"ba","bb","bd","be","bf","bg","bh","bi","bj",\
+		"bm","bn","bo","br","bs","bt","bv","bw","by",\
+		"bz","ca","cc","cd","cf","cg","ch","ci","ck",\
+		"cl","cm","cn","co","cr","cu","cv","cx","cy",\
+		"cz","de","dj","dk","dm","do","dz","ec","ee",\
+		"eg","er","es","et","eu","fi","fj","fk","fm",\
+		"fo","fr","ga","gb","gd","ge","gf","gg","gh",\
+		"gi","gl","gm","gn","gp","gq","gr","gs","gt",\
+		"gu","gw","gy","hk","hm","hn","hr","ht","hu",\
+		"id","ie","il","im","in","io","iq","ir","is",\
+		"it","je","jm","jo","jp","ke","kg","kh","ki",\
+		"km","kn","kp","kr","kw","ky","kz","la","lb",\
+		"lc","li","lk","lr","ls","lt","lu","lv","ly",\
+		"ma","mc","md","me","mg","mh","mk","ml","mm",\
+		"mn","mo","mp","mq","mr","ms","mt","mu","mv",\
+		"mw","mx","my","mz","na","nc","ne","nf","ng",\
+		"ni","nl","no","np","nr","nu","nz","om","pa",\
+		"pe","pf","pg","ph","pk","pl","pm","pn","pr",\
+		"ps","pt","pw","py","qa","re","ro","rs","ru",\
+		"rw","sa","sb","sc","sd","se","sg","sh","si",\
+		"sj","sk","sl","sm","sn","so","sr","st","su",\
+		"sv","sy","sz","tc","td","tf","tg","th","tj",\
+		"tk","tl","tm","tn","to","tp","tr","tt","tv",\
+		"tw","tz","ua","ug","uk","us","uy","uz","va",\
+		"vc","ve","vg","vi","vn","vu","wf","ws","ye",\
+		"yt","yt","za","zm","zw","xn--fiqs8s","xn--fiqz9s",\
+		"xn--wgbh1c","xn--j6w193g","xn--mgba3a4f16a",\
+		"xn--mgbayh7gpa","xn--ygbi2ammx","xn--p1ai",\
+		"xn--mgberp4a5d4ar","xn--fzc2c9e2c",\
+		"xn--xkc2al3hye2a","xn--kprw13d","xn--kpry57d",\
+		"xn--o3cw4h","xn--pgbs0dh","xn--mgbaam7a8h"]
+	
+	__adblock_rules_uri = []
+	__adblock_rules_uri_exceptions = []
+	__adblock_rules_element = []
+	__adblock_rules_element_exceptions = []
+	
 	def __init__(self,hostPID):
 		super(WebClient, self).__init__(hostPID)
 		self.set_title("blank page")
 		
+		self.parse_adblock_fitlers()
+		
+		pass
+	
+	def parse_adblock_fitlers(self):
+		f = open('/home/dan/Desktop/Programming/ude/ude-components/client/easylist.txt', 'r')
+		for line in f:
+			if line.find("!") != -1:
+				# Comment
+				pass
+			elif line.find("##") != -1:
+				if line.find("@@") != -1:
+					self.__adblock_rules_element_exceptions.append(line.strip())
+				else:
+					self.__adblock_rules_element.append(line.strip())
+			else:
+				if line.find("@@") != -1:
+					self.__adblock_rules_uri_exceptions.append(line.strip())
+				else:
+					self.__adblock_rules_uri.append(line.strip())
 		pass
 	
 	def prepare_new_widget(self):
@@ -81,9 +143,10 @@ class WebClient(ComponentClient):
 		
 		tb = gtk.ToolItem()
 		tb.set_expand(True)
-		im = gtk.image_new_from_file("16x16doc.svg")
+		im = gtk.image_new_from_file("/usr/share/ude/components/16x16doc.svg")
 		self.__uri_entry = sexy.IconEntry()
 		self.__uri_entry.set_icon(sexy.ICON_ENTRY_PRIMARY,im)
+		self.__uri_entry.connect("activate",self.__uri_entry_activate)
 		tb.add(self.__uri_entry)
 		self.toolbar.insert(tb,-1)
 		
@@ -101,6 +164,7 @@ class WebClient(ComponentClient):
 		self.__wv = WebViewTab()
 		self.__wv.connect("notify",self.__wv_notify)
 		self.__wv.connect("load-error",self.__wv_load_error)
+		self.__wv.connect("resource-request-starting",self.__wv_resource_request_starting)
 		
 		sw = gtk.ScrolledWindow()
 		sw.add(self.__wv)
@@ -110,6 +174,42 @@ class WebClient(ComponentClient):
 		
 		
 		return vbox
+	#http://www.google.ca/search?&q=pie
+	def __uri_entry_activate(self,sender):
+		text = self.__uri_entry.get_text().strip()
+		if text == None or text == "": return
+		
+		
+		validated_url = None
+		
+		scheme, netloc, path, params, query, fragment = urlparse.urlparse(text)
+		if scheme == '':
+			if netloc == '':
+				if path == '':
+					raise Exception("scheme netloc and path are all empty...")
+				else:
+					#
+					#
+					if path.startswith("www."):
+						validated_url = "http://"+text
+					else:
+						for tld in self.tlds:
+							if path.find("."+tld) != -1 and path.find(" ") == -1:
+								validated_url = "http://"+text
+						if None == validated_url:	
+							validated_url = "http://www.google.ca/search?&q="+urllib.quote_plus(text)
+			else:
+				validated_url = "http://"+text
+		else:
+			validated_url = text
+		
+		self.__uri_entry.set_text(validated_url)
+		self.__wv.load_uri(validated_url)
+		
+		window = self.__uri_entry.get_ancestor(gtk.Window.__gtype__)
+		if None != window: window.set_focus(self.__wv)
+		print "activate",text
+		pass
 	
 	def __tb_back_button_clicked(self,sender):
 		self.__wv.go_back()
@@ -174,20 +274,8 @@ class WebClient(ComponentClient):
 			self.set_title(title)
 		elif pspec.name == "icon-uri":
 			uri = self.__wv.get_property("icon-uri")
-			
 			filename, headers = urllib.urlretrieve(uri)
 			self.__favicon_path = filename
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			print "icon-uri",uri,"__favicon_path",self.__favicon_path
 		elif pspec.name in ["has-tooltip","has-focus","is-focus","parent","visible","style","window","events"]:
 			pass
 		else:
@@ -200,6 +288,45 @@ class WebClient(ComponentClient):
 	def __wv_load_error(self,sender,frame,arg,arg2):
 		print "__wv_load_error",self,sender,frame,arg,arg2
 	
+	def __wv_resource_request_starting(self,webview,web_frame,web_resource,request,response):
+		uri = request.get_uri()
+		#scheme, netloc, path, params, query, fragment = urlparse.urlparse(uri)
+		#scheme, netloc, path, params, query, fragment, username, password, hostname, port = \
+		#	urlparse.urlparse(request.get_uri())
+		
+		for rule in self.__adblock_rules_uri:
+			if self.__uri_matches_rule(uri,rule):
+				request.set_uri("about:blank")
+		
+		
+		
+		
+		
+		#if netloc == "www.google-analytics.com":
+		#	request.set_uri("about:blank")
+		
+		#file:///usr/share/ude/components/spacer.gif
+		
+		#print "__wv_resource_request_starting",scheme, netloc, path, params, query, fragment
+		pass
+	
+	def __uri_matches_rule(self,uri,rule):
+		
+		#print "__uri_matches_rule",rule
+		
+		if rule.find("*") != -1 or rule.find("^") != -1 or rule.find("|") != -1 or rule.find("~") != -1:
+			
+			pass
+		#elif regex:
+		else:
+			return self.__uri_matches_rule_simple(uri,rule)
+		
+		
+		return False
+	
+	def __uri_matches_rule_simple(self,uri,rule):
+		print rule
+		return False
 	
 class WebViewTab(webkit.WebView):
 	settings = None
@@ -222,7 +349,6 @@ class WebViewTab(webkit.WebView):
 		self.connect_after("populate-popup", self.populate_popup)
 		
 		self.load_uri("http://www.google.com")
-		#self.load_uri("http://tsunami/tmp")
 		
 	def populate_popup(self, view, menu):
 		pass
