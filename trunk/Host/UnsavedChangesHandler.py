47f69f06922e88f4b9fd79b7ff314e8cf03b615b
daniel.saul@gmail.com <daniel.saul@gmail.com@1d4a5069-0308-5a73-d597-13d25cdbd855>
2011-01-13 02:39:35 +0000

    Minor changes: - Renamed unsaved_changes_handler_return to unsaved_changes_handler_cb. - Added SAVE_STATUS_RANGE so that we can do assertions against this pseudo enum.
    Major changes:
    - Add assertions so that the code now knows the programmer's assumptions.
    - Add documentation to each function.
    
    TODO:
    - Add a customized display if all the tabs are Client.SAVE_STATUS_UNSAVABLE

--- trunk/Host/UnsavedChangesHandler.py
+++ trunk/Host/UnsavedChangesHandler.py
@@ -6,6 +6,8 @@ gobject.threads_init ()
 
 l = Host.logger
 
+# TODO! Add a customized display if all the tabs are Client.SAVE_STATUS_UNSAVABLE
+
 class UnsavedChangesHandler(object):
 	builder = None
 	window = None
@@ -19,8 +21,17 @@ class UnsavedChangesHandler(object):
 	delegate = None
 	
 	def __init__(self,clients_denying_close,delegate):
+		"""
+		Displays a window allowing the user to choose which tabs are saved, or if all of them are.
+		- clients_denying_close must have at least one item within it to ask the user.
+		- delegate must be not null, it must also respond to the following:
+		  - unsaved_changes_handler_cb(resolution)
+		"""
+		assert None != clients_denying_close
+		assert len(clients_denying_close) > 0
+		assert None != delegate
+		
 		super(UnsavedChangesHandler, self).__init__()
-		#Host.logger.debug("__init__(self={0},clients_denying_close={1},delegate={2})".format(self,clients_denying_close,delegate))
 		
 		self.builder = gtk.Builder()
 		self.builder.add_from_file(Host.glade_prefix+"UnsavedChangesHandler.glade")
@@ -28,48 +39,73 @@ class UnsavedChangesHandler(object):
 		self.button_save_all = self.builder.get_object("buttonSaveAll") 
 		self.button_cancel = self.builder.get_object("buttonCancel") 
 		self.button_dont_save = self.builder.get_object("buttonDontSave") 
-		self.vbox = self.builder.get_object("vbox") 
+		self.vbox = self.builder.get_object("vbox")
 		
-		self.clients_denying_close = clients_denying_close
+		assert None != self.builder
+		assert None != self.window
+		assert None != self.button_save_all
+		assert None != self.button_cancel
+		assert None != self.button_dont_save
+		assert None != self.vbox
+		
+		self.__clients_denying_close = clients_denying_close
 		self.delegate = delegate
 		
 		self.window.set_type_hint(gtk.gdk.WINDOW_TYPE_HINT_DIALOG)
 		self.window.set_deletable(False)
-		#self.window.set_resizable(False)
 		self.window.set_title("Save Changes?")
-		#self.window.set_decorated(False)
 		
-		for client in self.clients_denying_close:
+		for client in self.__clients_denying_close:
 			
 			# We need to make a manual save row for the client.
 			client.__unsaved_hbox = gtk.HBox(spacing=10)
+			assert None != client.__unsaved_hbox
+			
 			client.__unsaved_spinner = gtk.Spinner()
 			client.__unsaved_spinner.start()
 			client.__unsaved_spinner.set_size_request(22,22)
+			assert None != client.__unsaved_spinner
+			
 			client.__unsaved_image_save = gtk.image_new_from_stock(gtk.STOCK_FLOPPY,gtk.ICON_SIZE_BUTTON)
 			client.__unsaved_image_save.set_size_request(22,22)
+			assert None != client.__unsaved_image_save
+			
 			client.__unsaved_image_saved = gtk.image_new_from_stock(gtk.STOCK_APPLY,gtk.ICON_SIZE_BUTTON)
 			client.__unsaved_image_saved.set_size_request(22,22)
+			assert None != client.__unsaved_image_saved
+			
 			client.__unsaved_image_no_save = gtk.image_new_from_stock(gtk.STOCK_DIALOG_WARNING,gtk.ICON_SIZE_BUTTON)
 			client.__unsaved_image_no_save.set_size_request(22,22)
+			assert None != client.__unsaved_image_no_save
+			
 			client.__unsaved_label = gtk.Label("NO LABEL SET")
 			client.__unsaved_label.set_justify(gtk.JUSTIFY_LEFT)
 			client.__unsaved_label.set_alignment(0,0.5)
 			client.__unsaved_label.set_ellipsize(pango.ELLIPSIZE_END)
 			client.__unsaved_label.set_line_wrap(False)
 			client.__unsaved_label.set_selectable(True)
+			assert None != client.__unsaved_label
+			
 			client.__unsaved_button_dontsave = gtk.Button("Don't Save")
 			client.__unsaved_button_dontsave.connect("clicked",self.__cb_dont_save_specific_client,client)
 			client.__unsaved_button_dontsave.set_size_request(100,-1)
+			assert None != client.__unsaved_button_dontsave
+			
 			client.__unsaved_button_save = gtk.Button("Save")
 			client.__unsaved_button_save.connect("clicked",self.__cb_save_specific_client,client)
 			client.__unsaved_button_save.set_size_request(100,-1)
+			assert None != client.__unsaved_button_save
+			
 			client.__unsaved_button_save_as = gtk.Button("Save As...")
 			#client.__unsaved_button_save_as.connect("clicked",self.__cb_saveas_specific_client,client)
 			client.__unsaved_button_save_as.set_size_request(100,-1)
+			assert None != client.__unsaved_button_save_as
+			
 			client.__unsaved_button_cant_save = gtk.Button("Not Savable")
 			client.__unsaved_button_cant_save.set_size_request(210,-1)
 			client.__unsaved_button_cant_save.set_sensitive(False)
+			assert None != client.__unsaved_button_cant_save
+			
 			client.__unsaved_hbox.pack_start(client.__unsaved_spinner,expand=False)
 			client.__unsaved_hbox.pack_start(client.__unsaved_image_save,expand=False)
 			client.__unsaved_hbox.pack_start(client.__unsaved_image_saved,expand=False)
@@ -82,7 +118,12 @@ class UnsavedChangesHandler(object):
 			self.vbox.pack_start(client.__unsaved_hbox,expand=False)
 			
 			description = client.GetDescription()
-			client.__unsaved_label.set_markup(description.replace("&","&amp;"))
+			assert None != description
+			
+			description = description.replace("&","&amp;")
+			assert None != description
+			
+			client.__unsaved_label.set_markup(description)
 			
 			client.__unsaved_hbox.show()
 			client.__unsaved_label.show()
@@ -94,7 +135,23 @@ class UnsavedChangesHandler(object):
 		self.button_dont_save.connect("clicked",self.__cb_dont_save_all)
 	
 	def __update_client_status_visibility(self,client,status=None):
-		#l.debug("__update_client_status_visibility(self={0},client={1},status={2})".format(self,client,status))
+		"""
+		Manages the row for the specific client provided. Shows 
+		and/or hides things based upon the status argument. If the
+		status argument is not provided the client is queried for its
+		current status.
+		"""
+		assert None != client
+		assert None != client.__unsaved_hbox
+		assert None != client.__unsaved_spinner
+		assert None != client.__unsaved_image_save
+		assert None != client.__unsaved_image_saved
+		assert None != client.__unsaved_image_no_save
+		assert None != client.__unsaved_label
+		assert None != client.__unsaved_button_dontsave
+		assert None != client.__unsaved_button_save
+		assert None != client.__unsaved_button_save_as
+		assert None != client.__unsaved_button_cant_save
 		
 		if status==None:
 			status = client.GetSaveStatus()
@@ -140,66 +197,101 @@ class UnsavedChangesHandler(object):
 			client.__unsaved_button_cant_save.show()
 	
 	def show(self,parent):
-		#l.debug("show(self={0},parent={1})".format(self,parent))
+		"""
+		Show the unsaved changes handler. Requires a transient parent window. 
+		This would normally be the host window. This handler will be modal 
+		for the entire host widnow. 
+		"""
+		assert None != parent
+		assert None != self.window
+		assert None != self.__clients_denying_close
 		
 		self.window.set_modal(True)
 		self.window.set_transient_for(parent)
 		self.window.show()
 		
-		# Remove unsavable items from self.clients_denying_close 
+		# Remove unsavable items from self.__clients_denying_close 
 		# so they don't deny close. This has to be done after 
 		# showing so that they are visable in the list none the less.
 		new_arr = []
-		for client in self.clients_denying_close:
+		for client in self.__clients_denying_close:
 			status = client.GetSaveStatus()
 			if status != Client.SAVE_STATUS_UNSAVABLE:
 				new_arr.append(client)
 		
-		self.clients_denying_close = new_arr
-		pass
+		self.__clients_denying_close = new_arr
 	
 	def save_specific_client(self,client):
-		#l.debug("save_specific_client(self={0},client={1})".format(self,client))
+		"""
+		Tell the provided <client> to save itself.
+		"""
+		assert None != client
 		
 		client.Save()
 	
 	def dont_save_specific_client(self,client):
-		#l.debug("dont_save_specific_client(self={0},client={1})".format(self,client))
+		"""
+		Mark <client> as not requiring saving. The user will still have 
+		the option to manually choose to save it however by clicking save.
+		"""
+		assert None != client
+		assert None != client.__unsaved_button_dontsave
+		assert None != self.__clients_denying_close
+		assert None != self.window
+		assert None != self.delegate
 		
-		if client in self.clients_denying_close:
-			self.clients_denying_close.remove(client)
+		# Remove this client from the list so that it no longer denies close.
+		if client in self.__clients_denying_close:
+			self.__clients_denying_close.remove(client)
 		
 		client.__unsaved_button_dontsave.set_sensitive(False)
 		
-		if len(self.clients_denying_close) == 0:
+		# If there are no more tabs denying close, then return that everything has been "saved".
+		if len(self.__clients_denying_close) == 0:
 			self.window.hide()
-			self.delegate.unsaved_changes_handler_return(UnsavedChangesHandler.RETURN_SAVED_ALL)
+			self.delegate.unsaved_changes_handler_cb(UnsavedChangesHandler.RETURN_SAVED_ALL)
 	
 	def save_all(self):
-		#l.debug("save_all(self={0})".format(self))
+		"""
+		Iterate though all clients currently denying close and save them.
+		WARNING: If the user previously chose "Don't Save" that client WILL
+		NOT BE SAVED!!!
+		"""
+		assert None != self.__clients_denying_close
 		
-		for client in self.clients_denying_close:
+		for client in self.__clients_denying_close:
 			client.Save()
 	
 	def dont_save_all(self):
-		#l.debug("dont_save_all(self={0})".format(self))
+		"""
+		Tell the delegate that everything has been saved. Of course 
+		it hasn't, but that is what the user chose.
+		"""
+		assert None != self.window
+		assert None != self.delegate
 		
 		self.window.hide()
-		self.delegate.unsaved_changes_handler_return(UnsavedChangesHandler.RETURN_SAVED_ALL)
+		self.delegate.unsaved_changes_handler_cb(UnsavedChangesHandler.RETURN_SAVED_ALL)
 	
 	def cancel(self):
-		#l.debug("cancel(self={0})".format(self))
+		"""
+		Just close the unsaved changes window.
+		"""
+		assert None != self.window
+		assert None != self.delegate
 		
 		self.window.hide()
-		self.delegate.unsaved_changes_handler_return(UnsavedChangesHandler.RETURN_CANCEL)
+		self.delegate.unsaved_changes_handler_cb(UnsavedChangesHandler.RETURN_CANCEL)
 	
 	#
 	# BUTTON EVENT CALLBACKS
 	#
 	def __cb_save_specific_client(self,sender,client):
+		assert None != client
 		self.save_specific_client(client)
 		
 	def __cb_dont_save_specific_client(self,sender,client):
+		assert None != client
 		self.dont_save_specific_client(client)
 	
 	def __cb_save_all(self,sender):
@@ -219,22 +311,29 @@ class UnsavedChangesHandler(object):
 	# REMOTE NOTIFICATIONS
 	#
 	def update_client_status(self,client,status):
-		#l.debug("update_save_status(self={0},client={1},status={2})".format(self,client,status))
+		"""
+		Called to update the status. Used to update the display once the client has finished saving.
+		"""
+		assert None != client
+		assert None != self.__clients_denying_close
+		assert None != self.window
+		assert None != self.delegate
+		assert status in Client.SAVE_STATUS_RANGE
 		
 		self.__update_client_status_visibility(client,status)
+		
 		if status == Client.SAVE_STATUS_SAVED:
-			self.clients_denying_close.remove(client)
+			self.__clients_denying_close.remove(client)
 		
 		#print "update_client_status",client,status
-		if len(self.clients_denying_close) == 0:
+		if len(self.__clients_denying_close) == 0:
 			self.window.hide()
-			self.delegate.unsaved_changes_handler_return(UnsavedChangesHandler.RETURN_SAVED_ALL)
-		
-		pass
+			self.delegate.unsaved_changes_handler_cb(UnsavedChangesHandler.RETURN_SAVED_ALL)
+
 	
 UnsavedChangesHandler.RETURN_CANCEL = 0
 UnsavedChangesHandler.RETURN_SAVED_ALL = 1
-	
+UnsavedChangesHandler.RETURN_RANGE = range(UnsavedChangesHandler.RETURN_CANCEL,UnsavedChangesHandler.RETURN_SAVED_ALL+1)
 	
 	
 	
